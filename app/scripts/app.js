052e39626a484f6b1e6dc337485a213263b8451d
gick <gicquel.pierreyves@gmail.com>
2016-03-31 23:44:01 +0200

    Lot if cganfes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,14 +8,14 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 */
 
 function distance(lat1, lon1, lat2, lon2, unit) {
-  var radlat1 = Math.PI * lat1/180
-  var radlat2 = Math.PI * lat2/180
-  var theta = lon1-lon2
-  var radtheta = Math.PI * theta/180
+  var radlat1 = Math.PI * lat1/180;
+  var radlat2 = Math.PI * lat2/180;
+  var theta = lon1-lon2;
+  var radtheta = Math.PI * theta/180;
   var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-  dist = Math.acos(dist)
-  dist = dist * 180/Math.PI
-  dist = dist * 60 * 1.1515
+  dist = Math.acos(dist);
+  dist = dist * 180/Math.PI;
+  dist = dist * 60 * 1.1515;
   if (unit=="K") { dist = dist * 1.609344 }
   if (unit=="N") { dist = dist * 0.8684 }
   return dist
@@ -29,7 +29,7 @@ function distance(lat1, lon1, lat2, lon2, unit) {
   var app = document.querySelector('#app');
 
   // Sets app default base URL
-  app.baseUrl = '/';
+  app.baseUrl = '/reveries';
   if (window.location.port === '') {  // if production
     // Uncomment app.baseURL below and
     // set app.baseURL to '/your-pathname/' if running from folder in production
